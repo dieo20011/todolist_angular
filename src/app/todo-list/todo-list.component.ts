64c91e18fbee3e984b97ed5d855db287a718ad25
Duy Luan <phamduyluan102001@gmail.com>
2023-10-17 22:09:52 +0700

    pathvalue to edit to input

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -9,13 +9,17 @@ import {FormBuilder,FormGroup,Validators, AbstractControl, ValidatorFn} from '@a
 export class TodoListComponent {
   listItems: string[] = ['Clean house', 'Do Homework'];
   todoForm: FormGroup;
+  selectedItem: string | null = null;
   
   constructor(private formBuilder: FormBuilder){
     this.todoForm = this.formBuilder.group({
       newItem: ['',[Validators.required, this.whiteSpaceValidator]]
     })
   }
-  
+  editItem(item:string){
+    this.selectedItem = item;
+    this.todoForm.patchValue({newItem: item});
+  }
   addItem(){
     const valueItem = this.todoForm.get('newItem');
     if(valueItem && valueItem.valid){
